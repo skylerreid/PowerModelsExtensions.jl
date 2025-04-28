1d079dc0c62745e7bda3906f18542835e6d77ec1
Skyler Reid <96967651+skylerreid@users.noreply.github.com>
2025-04-28 11:03:44 -0600

    Add files via upload
    Initial upload of src folder and Project.toml file

---
+++ src/PowerModelsExtensions.jl
@@ -0,0 +1,13 @@
+module PowerModelsExtensions
+
+__precompile__()
+
+using PowerModels, MathOptInterface, JuMP, Ipopt
+
+include("find_qg_limit.jl")
+include("find_pg_limit.jl")
+include("modify_loads.jl")
+include("modify_gens.jl")
+include("loadtogenratio.jl")
+
+end

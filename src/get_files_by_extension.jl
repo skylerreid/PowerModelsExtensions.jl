76ddc9c0adecaf8d71cbd0fe4533a89a8424c40a
Reid <409610@win.lanl.gov>
2025-09-15 09:24:02 -0600

    modified get files function to sort in alphabetical order

--- src/get_files_by_extension.jl
+++ src/get_files_by_extension.jl
@@ -1,5 +1,5 @@
 function get_files_by_extension(directory_path::String, extension::String)
     all_files_and_dirs = readdir(directory_path, join = true)
     filtered_files = filter(x -> endswith(lowercase(x), lowercase(extension)), all_files_and_dirs)
-    return filtered_files
+    return sort(filtered_files)
 end

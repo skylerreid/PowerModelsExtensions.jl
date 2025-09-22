f81e9541ec174d14be94fb57c51e1ef4b9ac6677
Skyler Reid <96967651+skylerreid@users.noreply.github.com>
2025-09-22 11:03:25 -0600

    Update ipopt_macro.jl

--- src/ipopt_macro.jl
+++ src/ipopt_macro.jl
@@ -1,15 +1,21 @@
 # optimizer_macros.jl
 
 """
-    @ipopt
+    @ipopt [tolerance]
 
 Creates an IPOPT solver with predefined settings.
+Optional parameter:
+- `tolerance`: Convergence tolerance (default: 1e-4)
+
+Examples:
+    @ipopt         # Uses default tolerance of 1e-3
+    @ipopt 1e-2    # Sets tolerance to 1e-2
 """
-macro ipopt()
+macro ipopt(tolerance=1e-3)
     return esc(quote
         ipopt_solver = JuMP.optimizer_with_attributes(
             Ipopt.Optimizer,
-            "tol" => 1e-4,
+            "tol" => $tolerance,
             "print_level" => 0,
             "sb" => "yes"
         )
